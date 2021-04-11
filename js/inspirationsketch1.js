7f73e18a5a4aff809a83b4f9361942ca96fc1754
wilhsu <hsuw073@newschool.edu>
2021-04-12 00:06:35 +0800

    inspiration sketch 1

--- js/inspirationsketch1.js
+++ js/inspirationsketch1.js
@@ -1,9 +1,20 @@
 function setup(){
-	createCanvas(1000,1000);
+	createCanvas(400,400);
 }
 function draw(){
 	background("gray");
 	noStroke();
-	fill("red");
-	rectangle(200,375,600,250);
+	fill("#D22B2B");
+	rect(42,147,315,175);
+	fill("black");
+	rect(170,155,47,31);
+	fill("white");
+	rect(270,153,52,25);
+	stroke(3);
+	noFill();
+	ellipse(170,240,92,92);
+	fill("black");
+	ellipse(170,240,40,40);
+	rect(265,222,54,20);
+	rect(115,139,36,8);
 }
